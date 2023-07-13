a179fcfbfbe1bb8755c70a5a75ae84ea622dd757
rubens.marques <rubens.marques@hotmart.com>
2023-07-13 14:43:21 -0300

    removing arquivos old

--- assets-old/js/script.js
+++
@@ -1,8 +0,0 @@
-import initScroll from './module/scroll-suave.js';
-import initHidden from './module/hidden.js';
-import initTyping from './module/anime-typing.js';
-
-initScroll();
-initHidden();
-initTyping()
-

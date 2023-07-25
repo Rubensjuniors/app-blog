3d1b1681e54ab102c18635ceb7ca3f25b40b636d
rubens.marques <rubens.marques@hotmart.com>
2023-07-25 19:05:58 -0300

    branch teste

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import router from './Routes/routes'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </>
 )

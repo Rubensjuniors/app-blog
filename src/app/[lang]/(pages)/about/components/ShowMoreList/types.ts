e7a64aca6dec2aa02fb689bf7beab0968d67d876
rubens.marques <rubens.marques@hotmart.com>
2023-10-01 09:59:19 -0300

    refactor: showMoreList

---
+++ src/app/[lang]/(pages)/about/components/ShowMoreList/types.ts
@@ -0,0 +1,4 @@
+export interface ShowMoreListProps {
+  list: string[] | []
+  showIndice: number
+}
--- src/app/[lang]/(pages)/about/page.tsx
+++ src/app/[lang]/(pages)/about/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import Image from 'next/image'
-import { useState } from 'react'
 
 import { useTranslationClient } from '@/hooks/useTransletions/client'
 
@@ -8,11 +7,10 @@ import { Icon, Card } from '@/components/basic'
 
 import bannerProfile from '../../../../../public/assets/img/photos/banner.jpeg'
 import photoProfile from '../../../../../public/assets/img/photos/Photo_three.jpg'
+import ShowMoreList from './components/ShowMoreList'
 
 const About = () => {
   const t = useTranslationClient()
-  const [showMore, setShowMore] = useState<boolean>(false)
-
   const textAboutMe = [
     t.blog.about.about.text.p1,
     t.blog.about.about.text.p2,
@@ -71,37 +69,7 @@ const About = () => {
           title={t.blog.about.about.title}
           classNames="p-4 flex flex-col items-start gap-2"
         >
-          {textAboutMe.map((text, index) => {
-            const showMoreText = (index < 1 && !showMore) || showMore
-            return (
-              showMoreText && (
-                <p
-                  key={text}
-                  className="text-lg-xl mb-2 text-justify text-lg font-light"
-                >
-                  {text}
-                </p>
-              )
-            )
-          })}
-          <button
-            className="cursor-pointer underline md:hover:text-red-300"
-            onClick={() => setShowMore(!showMore)}
-          >
-            <span className="flex items-center gap-2">
-              {showMore ? (
-                <>
-                  {t.blog.about.about.showsLess}
-                  <Icon id="caretUp-phosphor" iconSize={18} />
-                </>
-              ) : (
-                <>
-                  {t.blog.about.about.showMore}
-                  <Icon id="caretDown-phosphor" iconSize={18} />
-                </>
-              )}
-            </span>
-          </button>
+          <ShowMoreList list={textAboutMe} showIndice={1} />
         </Card>
       </section>
     </>

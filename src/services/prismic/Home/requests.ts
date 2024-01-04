cc13b1e7a3a87d38f84abdf654dbf8515f20b26f
rubens.marques <rubens.marques@hotmart.com>
2024-01-04 10:11:26 -0300

    feat: added try and catch in all requests

--- src/services/prismic/Home/requests.ts
+++ src/services/prismic/Home/requests.ts
@@ -1,30 +1,36 @@
 import { getPrismicClient } from '@/services/prismic'
 
 export async function getHomeData() {
-  const prismic = getPrismicClient()
+  try {
 
-  const responseHome = await prismic?.getByType('home', {
-    fetch: [
-      'home.photo_profile',
-      'home.title',
-      'home.description',
-      'home.latest_articles',
-    ],
-    pageSize: 100,
-  })
-  const { results } = responseHome
-  const home = {
-    photoProfile:{
-      url:  results[0].data.photo_profile.url,
-      width:  results[0].data.photo_profile.dimensions.width,
-      heigth:  results[0].data.photo_profile.dimensions.heigth
-    },
-    title: results[0].data.title,
-    description: results[0].data.description,
-    latest_articles: results[0].data.latest_articles
-  }
+    const prismic = getPrismicClient()
 
-  return {
-    home
+    const responseHome = await prismic?.getByType('home', {
+      fetch: [
+        'home.photo_profile',
+        'home.title',
+        'home.description',
+        'home.latest_articles',
+      ],
+      pageSize: 100,
+    })
+    const { results } = responseHome
+    const home = {
+      photoProfile:{
+        url:  results[0].data.photo_profile.url,
+        width:  results[0].data.photo_profile.dimensions.width,
+        heigth:  results[0].data.photo_profile.dimensions.heigth
+      },
+      title: results[0].data.title,
+      description: results[0].data.description,
+      latest_articles: results[0].data.latest_articles
+    }
+
+    return {
+      home
+    }
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Failed fetching Home', error)
   }
 }

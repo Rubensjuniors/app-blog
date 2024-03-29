85da5ad00a732ac104ae87341730586e181281f7
rubens.marques <rubens.marques@hotmart.com>
2024-03-29 09:26:51 -0300

    feat: added button back in page post and responsive banner

--- src/components/Structore/Sidemenu/index.tsx
+++ src/components/Structore/Sidemenu/index.tsx
@@ -17,7 +17,7 @@ const Sidemenu = ({ sidemenuItems }: sidemenuProps) => {
   return (
     <nav
       data-testid="sidemenu"
-      className="sticky left-0 top-0 hidden max-h-screen w-[73px] overflow-hidden flex-col items-center justify-start gap-4 p-3 pt-5 sm1:flex lg:hover:w-56 lg:items-start lg:p-1 lg:pt-5 duration-500 ease-in-out"
+      className="sticky left-0 top-0 hidden max-h-screen w-[73px] overflow-hidden flex-col items-center justify-start gap-4 p-3 pt-5 lg:flex lg:hover:w-56 lg:items-start lg:p-1 lg:pt-5 duration-500 ease-in-out"
     >
       <Link href="/" className="lg:pl-2">
         <Icon id="icon_logo" iconSize={42} />

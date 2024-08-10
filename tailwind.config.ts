036324ed31c176ace3b401a917a85d71ca113434
rubens.marques <rubens.marques@hotmart.com>
2024-08-10 11:00:25 -0300

    feat: added configs css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,50 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+      }
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: 'var(--font-roboto)',
+        'marck-script': 'var(--font-marck-script)',
+      },
+      colors: {
+        'gray-100': '#E1E1E6',
+        'gray-300': '#C4C4CC',
+        'gray-400': '#8D8D99',
+        'gray-500': '#7C7C8A',
+        'gray-600': '#323238',
+        'gray-700': '#29292E',
+        'gray-750': '#2F2F2F',
+        'gray-800': '#202024',
+        'gray-900': '#121214',
+
+        'green-300': '#00B37E',
+        'green-500': '#00875F',
+        'green-700': '#015F43',
+
+        'red-300': '#F75A68',
+        'red-500': '#AB222E',
+        'red-700': '#7A1921',
+      },
+      screens: {
+        sm: '576px',
+        // => @media (min-width: 576px) { ... }
+
+        md: '768px',
+        // => @media (min-width: 768px) { ... }
+
+        lg: '960px',
+        // => @media (min-width: 960px) { ... }
+
+        xl: '1440px',
+        // => @media (min-width: 1440px) { ... }
       },
     },
   },

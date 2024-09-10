c191472e0e2c39af01bc31ac949eff83947c8594
rubens.marques <rubens.marques@hotmart.com>
2024-09-10 09:34:07 -0300

    feat: created card Post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,60 @@
+/* eslint-disable max-len */
+import CardPost from '@/components/Basic/CardPost'
 import CardProfile from '@/components/Home/CardProfile'
 
-const Home = () => (
-  <>
-    <CardProfile />
-  </>
-)
+const Home = () => {
+  const post = [
+    {
+      title: 'Como o TypeScript ajuda você a escrever um código melhor',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio laborum omnis voluptate. Dolores, magni quia totam ipsam quas cum earum quis necessitatibus voluptatum alias suscipit modi ab ullam hic nobis?',
+      tags: ['ss', 'ssda'],
+      publicationDate: '12/12/12',
+      uid: 'sasdasd',
+      type: 'code',
+      image: 'image.png'
+    },
+    {
+      title: 'Um title',
+      description: 'lorema asdkjaskldjlkajsdlk',
+      tags: ['ss', 'ssda'],
+      publicationDate: '12/12/12',
+      uid: 'sasdasd',
+      type: 'code',
+      image: 'image.png'
+    },
+    {
+      title: 'Um title',
+      description: 'lorema asdkjaskldjlkajsdlk',
+      tags: ['ss', 'ssda'],
+      publicationDate: '12/12/12',
+      uid: 'sasdasd',
+      type: 'code',
+      image: 'image.png'
+    }
+  ]
+
+  return (
+    <div className="p-5">
+      <CardProfile />
+
+      <div>
+        <h1 className="title mt-6">Mais recentes</h1>
+      </div>
+
+      {post.map((p) => (
+        <CardPost
+          key={p.uid}
+          image={p.image}
+          title={p.title}
+          description={p.description}
+          tags={p.tags}
+          publicationDate={p.publicationDate}
+          uid={p.uid}
+        />
+      ))}
+    </div>
+  )
+}
 
 export default Home
---
+++ src/components/Basic/CardPost/index.tsx
@@ -0,0 +1,76 @@
+/* eslint-disable max-len */
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Icon } from '..'
+
+import { PostProps } from './types'
+
+const flexCenter = 'flex items-center'
+
+const CardPost = ({
+  description,
+  publicationDate,
+  tags,
+  title,
+  path,
+  uid,
+  image
+}: PostProps) => {
+  const isTagsClass =
+    tags.length > 0 &&
+    'before:w-[2px] before:h-8 before:bg-gray-750 before:rounded-full'
+
+  return (
+    <div className="border-2 border-transparent border-b-gray-600 py-4 last:border-0">
+      <Link href={path ?? `/artigos/${uid}`} className="md:hover:brightness-75">
+        <div className="flex flex-col gap-2 sm:flex-row sm:gap-3">
+          <div
+            className={`${!image && 'hidden'} h-52 sm:max-h-36 sm:max-w-36`}
+          >
+            <Image
+              className="h-full rounded-md object-cover object-center"
+              src={`/assets/images/${image}`}
+              width={80}
+              height={80}
+              alt="foto de perfil"
+              title="foto de perfil"
+              priority
+            />
+          </div>
+
+          <div className="w-full">
+            <div className={`${flexCenter} w-full justify-between`}>
+              <h1 className="line-clamp-2 text-xl font-bold sm:text-2xl">
+                {title}
+              </h1>
+            </div>
+
+            <p className="text-md mb-4 mt-2 line-clamp-3 break-all font-light tracking-wide text-gray-300 sm:mt-1 sm:line-clamp-2">
+              {description}
+            </p>
+          </div>
+        </div>
+      </Link>
+
+      <div className={`${flexCenter} mt-2 gap-5 text-gray-300 sm:mt-3`}>
+        <ul className={`${flexCenter} gap-4`}>
+          <li className={`${flexCenter} gap-2`}>
+            <Icon id="calendarBlank-phosphor" />
+            <span>{publicationDate}</span>
+          </li>
+
+          <li className={` ${flexCenter} ${isTagsClass} hidden gap-5 sm:flex`}>
+            {tags.map((tag, index) => {
+              const limitTags = index < 3
+
+              return limitTags && <span key={index}>{tag}</span>
+            })}
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default CardPost

be012c0f63201976cd2f4610510156ab74305f40
rubens junio <rubens.marques@hotmart.com>
2024-12-10 20:07:45 -0300

    fix: fixing husky

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig, type UserConfigExport } from 'vite'
-import Inspect from 'vite-plugin-inspect'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), Inspect()],
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
